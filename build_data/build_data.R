31c43428ab6d6de3dfb453208530e0d3b3f4ade8
peeter-t2 <peeter.tinits@gmail.com>
2019-04-11 12:33:32 +0300

    init

---
+++ build_data/build_data.R
@@ -0,0 +1,43 @@
+library(data.table)
+library(tidyverse)
+
+setwd(here::here("build_data"))
+
+laulud2 <- fread("data/top40_metadata.csv")
+
+filelist <- list.files("data/songs/",full.names=T)
+
+for (file in filelist){
+  
+contents <- read_lines(file)
+write_lines(contents,file)
+
+}
+
+texts <- map_df(filelist, ~ data_frame(txt = read_lines(.x)) %>%
+                  mutate(filename = .x)) %>%
+  mutate(filename= gsub("data/songs//","",filename)) %>%
+  mutate(filename= gsub("\\.txt$","",filename))
+
+laulud <- texts%>%
+  mutate(txt=trimws(txt))%>%
+  filter(txt!="")%>%
+  filter(txt!="\"")%>%
+  group_by(filename) %>% 
+  filter(row_number()!=1) %>% #remove artist name
+  filter(row_number()!=1) %>% #remove song title
+  mutate(txt=trimws(gsub("\"$","",txt))) %>% #
+  summarize(txt=paste(txt, collapse=" "))
+
+lyrics <- merge(laulud2,laulud,by="filename",all=T)
+
+names(lyrics) <- c("filename","artist_title","year","keep","rank","votes","artist","song","source","lyrics")
+
+lyrics_clean <- lyrics[order(year,rank)][,.(year,rank,votes,artist,song,filename,source,lyrics)]
+
+fwrite(lyrics_clean,"../eesti_skyplus_top40_1994-2018.tsv",sep="\t")
+
+#basic stats
+nrow(lyrics_clean[!is.na(lyrics)])
+nrow(unique(lyrics_clean[,.(filename)]))
+                     
---
+++ readme.md
@@ -0,0 +1,29 @@
+2019, Peeter Tinits (CC-BY-4.0)
+
+# Skyplus Eesti top 40 laulusõnade korpus 1994-2018
+
+Andmestik sisaldab Sky Plus Eesti top 40 hulka jõudnud laulude sõnu. Top 40 on koostatud bändi päritolu järgi ja ei sisalda ainult eestikeelseid lugusid. Andmestik on mõeldud eelkõige lihtsama tekstitöötluse harjutamiseks.
+
+Nimistik on kogutud [Sky Plus aastahitt kodulehelt](https://r2.err.ee/881202/vaata-ja-kuula-uuesti-aastahitt-2018/912643) Andmed on kogutud [Sasslantisest](http://sasslantis.ee/) ning bändide kodulehtedelt.
+
+## Andmete struktuur
+
+- year - edetabeli aasta
+- rank - koht edetabelis
+- artist - esineja
+- song - loo pealkiri
+- filename - faili nimi hoidlas
+- source - sõnade allikas (1 - Sasslantis, 2 - muu)
+- lyrics - laulusõnad
+
+Sõnad on olemas 822 edetabeli laulule tuhandest, sh 756 eraldi laulule (mõni laul sai tabelisse mitu aastat).
+
+
+## Viide
+
+Andmestikule saab viidata:
+- Tinits, Peeter (2019). SkyPlus Eesti top 40 laulusõnade korpus 1994-2018.
+
+-----------------------
+
+Andmestik on [Creative Commons Attribution 4.0 International](https://creativecommons.org/licenses/by/4.0/) litsentsiga, lähtekood on [MIT](https://choosealicense.com/licenses/mit/) litsentsiga.
